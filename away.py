4cd7c2598a1bc44ffabd0c582bdc860827d54110
b0nn133 <bonnieroxtigers@yandex.ru>
2026-03-01 10:51:52 +0300

    Initial commit

---
+++ away.py
@@ -0,0 +1,125 @@
+# ---------------------------------------------------------------------------------
+# ██████  ██    ██ ███    ██ ███    ██ ██    ██ ███    ███  ██████  ██████  ██    ██ ██      ███████ ███████
+# ██   ██ ██    ██ ████   ██ ████   ██  ██  ██  ████  ████ ██    ██ ██   ██ ██    ██ ██      ██      ██
+# ██████  ██    ██ ██ ██  ██ ██ ██  ██   ████   ██ ████ ██ ██    ██ ██   ██ ██    ██ ██      █████   ███████
+# ██   ██ ██    ██ ██  ██ ██ ██  ██ ██    ██    ██  ██  ██ ██    ██ ██   ██ ██    ██ ██      ██           ██
+# ██████   ██████  ██   ████ ██   ████    ██    ██      ██  ██████  ██████   ██████  ███████ ███████ ███████
+#
+# Name: Away
+# Description: Send a message when you're offline
+# ---------------------------------------------------------------------------------
+# 🔒    Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# ---------------------------------------------------------------------------------
+# Author: @BunnyModules
+# Commands: away
+# scope: hikka_only
+# meta developer: @BunnyModules
+# ---------------------------------------------------------------------------------
+
+from telethon.tl.types import (
+    UserStatusOffline,
+    UserStatusLastWeek,
+    UserStatusLastMonth,
+)
+from .. import loader, utils
+
+@loader.tds
+class AwayAutoReplyMod(loader.Module):
+    """Auto-reply when away mode is enabled"""
+
+    strings = {
+        "name": "Away",
+        "on": "🟢 <b>Away enabled</b>",
+        "off": "🔴 <b>Away disabled</b>",
+    }
+
+    strings_ru = {
+        "name": "Away",
+        "on": "🟢 <b>Away включён</b>",
+        "off": "🔴 <b>Away выключен</b>",
+    }
+
+    strings_en = {
+        "name": "Away",
+        "on": "🟢 <b>Away enabled</b>",
+        "off": "🔴 <b>Away disabled</b>",
+    }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "reply_dm",
+                True,
+                "Auto-reply in private messages",
+                validator=loader.validators.Boolean(),
+            ),
+            loader.ConfigValue(
+                "reply_reply",
+                True,
+                "Auto-reply to replies in groups/channels",
+                validator=loader.validators.Boolean(),
+            ),
+            loader.ConfigValue(
+                "message",
+                "I'm currently offline. Last seen: {lastseen}.",
+                "Auto-reply message",
+                validator=loader.validators.String(),
+            ),
+        )
+
+        self.away = False
+        self._replied = set()
+
+    async def client_ready(self, client, db):
+        self._client = client
+
+    @loader.command()
+    async def away(self, message):
+        """Toggle away mode"""
+        self.away = not self.away
+        self._replied.clear()
+
+        await utils.answer(
+            message,
+            self.strings("on") if self.away else self.strings("off"),
+        )
+
+    async def _last_seen(self):
+        me = await self._client.get_me()
+        status = me.status
+
+        if isinstance(status, UserStatusOffline):
+            return status.was_online.strftime("%d.%m.%Y %H:%M")
+        if isinstance(status, UserStatusLastWeek):
+            return "last week"
+        if isinstance(status, UserStatusLastMonth):
+            return "a long time ago"
+
+        return "just now"
+
+    @loader.watcher()
+    async def watcher(self, message):
+        if not self.away:
+            return
+
+        if message.out or not message.sender_id:
+            return
+
+        key = (message.chat_id, message.sender_id)
+
+        if key in self._replied:
+            return
+
+        if message.is_private and self.config["reply_dm"]:
+            last = await self._last_seen()
+            await message.reply(self.config["message"].replace("{lastseen}", last))
+            self._replied.add(key)
+            return
+
+        if self.config["reply_reply"] and message.reply_to:
+            reply = await message.get_reply_message()
+            if reply and reply.sender_id == (await self._client.get_me()).id:
+                last = await self._last_seen()
+                await message.reply(self.config["message"].replace("{lastseen}", last))
+                self._replied.add(key)
